e2ae1e8ae31b21109fed443f2eb8f8ac218b4d58
akshaii7 <akshaistackup@gamil.com>
2026-05-09 21:05:42 +0530

    Updates

--- _next/static/o2bTUkPRm4Qbh-7dqUqOg/_buildManifest.js
+++ _next/static/o2bTUkPRm4Qbh-7dqUqOg/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/tour_bee//_next/:path+",
-        "destination": "/tour_bee/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
